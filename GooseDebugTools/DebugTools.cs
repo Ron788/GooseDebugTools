e4392ff337149acdadaa09eb0fd7bd04277b89fa
Ron788 <ustinov200511@gmail.com>
2021-07-08 14:12:56 +0300

    Fix

--- GooseDebugTools/DebugTools.cs
+++ GooseDebugTools/DebugTools.cs
@@ -14,16 +14,16 @@ namespace GooseDebugTools
         private static Color _background = Color.FromArgb(255, Color.Black),
             _text = ColorTranslator.FromHtml("#4AF626");
 
-        public static SolidBrush _brushBlack = new SolidBrush(_background),
-            _brushGreen = new SolidBrush(_text);
+        public static SolidBrush _brushBackground = new SolidBrush(_background),
+            _brushText = new SolidBrush(_text);
 
         public static void OutputLine(GooseEntity g, Graphics graph, int lineNum, string txt)
         {
             Size txtLen = graph.MeasureString(txt, SystemFonts.DefaultFont).ToSize();
             PointF txtPos = new PointF(g.position.x + 25, g.position.y + 25 + 14 * lineNum);
 
-            graph.FillRectangle(_brushBlack, new Rectangle(Point.Round(txtPos), txtLen));
-            graph.DrawString(txt, SystemFonts.DefaultFont, _brushGreen, txtPos);
+            graph.FillRectangle(_brushBackground, new Rectangle(Point.Round(txtPos), txtLen));
+            graph.DrawString(txt, SystemFonts.DefaultFont, _brushText, txtPos);
 
             lineNum++;
         }
